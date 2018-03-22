5a78655e090699459d977aa19f7b430f4bc2b8a3
MJChku <u3533156@hku.hk>
2018-03-22 21:39:29 +0800

    start app upload

---
+++ imageX/upload/forms.py
@@ -0,0 +1,9 @@
+from search.models import Image
+from django import forms
+
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'tag', 'image')
+
--- imageX/upload/views.py
+++ imageX/upload/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ImageForm
 
-# Create your views here.
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload')
+    else:
+        form = ImageForm()
+    return render(request, 'upload/model_form_upload.html', {'form': form} )
