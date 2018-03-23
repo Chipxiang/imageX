0474d09df3e804139e856766e6916a6a5d654f6b
MJChku <u3533156@hku.hk>
2018-03-23 15:17:32 +0800

    :)

--- imageX/search/views.py
+++ imageX/search/views.py
@@ -3,15 +3,14 @@ from django.shortcuts import render
 from upload.models import Image
 
 def index(request):
-    return render(request, 'search/index.html')
+    return render(request, 'search/searchBytags.html')
 
-def details(request):
-    return HttpResponse("hello world")
 
 def results(request):
     keyword = request.GET['searchItem']
     list_images = Image.objects.all()
     context = {'list_images': list_images, 'keyword': keyword}
-    return render(request, 'search/result.html', context)
+    #return HttpResponse("sucess")
+    return render(request, 'search/result.html',context)
 
 
