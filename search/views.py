2c2fdc5a5d161aae57d1998187dce667c3abf69f
Chipxiang <jianxiangli1996@gmail.com>
2018-03-27 17:42:15 +0800

    Fixed previous merge bugs and rearranged the search page

--- search/views.py
+++ search/views.py
@@ -11,16 +11,9 @@ from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 
 def searchImage(request):
-
-
     limit = 2
-
     list_images = []
-
-
     base_images = Image.objects.order_by('-uploaded_at')
-    base_images = Image.objects.order_by('-uploaded_at')
-
     nothing = False
     p=Paginator(base_images, limit)
     page = request.GET.get('page')
@@ -31,59 +24,18 @@ def searchImage(request):
     except EmptyPage:
         base_images = p.page(paginator.num_pages)
 
-    p=Paginator(base_images, limit)
-
-    page = request.GET.get('page')
-
-    try:
-
-        base_images = p.page(page)
-
-    except PageNotAnInteger:
-
-        base_images = p.page(1)
-
-    except EmptyPage:
-
-        base_images = p.page(paginator.num_pages)
-
-
-
     if 'searchItem' in request.GET:
-
         keyword = request.GET['searchItem']
-
-
         list_images = Image.objects.filter(tag__icontains=keyword).order_by('-uploaded_at')
-
-        #list_images = Image.objects.filter(tag=keyword)
-
-
-        list_images = Image.objects.filter(tag__icontains=keyword).order_by('-uploaded_at')
-        #list_images = Image.objects.filter(tag=keyword)
-
         if not list_images:
-
             nothing = True
-
         paginator = Paginator(list_images, limit)
-
         page = request.GET.get('page')
-
         try:
-
            list_images = paginator.page(page)
-
         except PageNotAnInteger:
-
            list_images = paginator.page(1)
-
         except EmptyPage:
-
            list_images = paginator.page(paginator.num_pages)
-
-
-
     context = {'list_images':list_images, 'base_images':base_images,'nothing':nothing, }
-
     return render(request, 'search/searchByTags.html', context)
