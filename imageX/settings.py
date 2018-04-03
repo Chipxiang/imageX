9532ec0135729921ed9093b06c0dcba7d85fb396
MJChku <u3533156@hku.hk>
2018-04-03 14:55:12 +0800

    deploymentsettings

--- imageX/settings.py
+++ imageX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@w*-5j2%%m6p#b-vsjzxxhb4fkobxpx=3*&4_@#efe%4qgjt(&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
